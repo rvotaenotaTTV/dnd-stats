f935f38f1048a3a0238545fc88c9b679a2e55a7f
rvotaenotaTTV <larcevk@gmail.com>
2026-04-21 14:08:25 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,7 @@
-// Service Worker для обхода блокировки raw.githubusercontent.com
 self.addEventListener('fetch', function(event) {
     const url = event.request.url;
     
-    // Если запрос к raw.githubusercontent.com
     if (url.includes('raw.githubusercontent.com')) {
-        // Меняем на jsDelivr
         const newUrl = url
             .replace('raw.githubusercontent.com/rvotaenotaTTV/dnd-stats/main', 'cdn.jsdelivr.net/gh/rvotaenotaTTV/dnd-stats@main');
         
