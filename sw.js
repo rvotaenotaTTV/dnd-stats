a429e9961f3eee863e14df1260d4c9acbecfdc75
rvotaenotaTTV <larcevk@gmail.com>
2026-04-21 15:10:35 +0300

    Auto update 2026-04-21 15:10

--- sw.js
+++ sw.js
@@ -2,8 +2,14 @@ self.addEventListener('fetch', function(event) {
     const url = event.request.url;
     
     if (url.includes('raw.githubusercontent.com')) {
-        const newUrl = url
-            .replace('raw.githubusercontent.com/rvotaenotaTTV/dnd-stats/main', 'cdn.jsdelivr.net/gh/rvotaenotaTTV/dnd-stats@main');
+        let newUrl = url.replace(
+            'raw.githubusercontent.com/rvotaenotaTTV/dnd-stats/main',
+            'cdn.jsdelivr.net/gh/rvotaenotaTTV/dnd-stats@main'
+        );
+        
+        if (newUrl.includes('.json')) {
+            newUrl += '?v=' + Date.now();
+        }
         
         console.log('[SW] Перенаправление:', url, '→', newUrl);
         event.respondWith(fetch(newUrl));
